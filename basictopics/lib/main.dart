aa1f38f8f174488577e41db3911cee6410ba244b
sajjad-njr <maxsajjad29@gmail.com>
2022-08-21 22:51:36 +0600

    Button Color add

--- basictopics/lib/main.dart
+++ basictopics/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
-import 'BottomNavigationChange.dart';
-import 'app_bar_title.dart';
-import 'bottom_nevigation.dart';
-import 'button_create.dart';
+
+//import 'ListOfButton.dart';
+import 'designButton.dart';
+//import 'BottomNavigationChange.dart';
+//import 'app_bar_title.dart';
+//import 'bottom_nevigation.dart';
+//import 'button_create.dart';
 
 void main() {
   //runApp(const app_bar_title()); //-------------1st call
   //runApp(const bottom_nevigation()); //-------- 2nd call
   //runApp(button_create()); //--------------- 3rd call
-  runApp(BottomNavigationChange());
+  //runApp(BottomNavigationChange()); //------------- 4th call
+  //runApp(ListOfButton()); //-------------------------- 5th call
+  runApp(designButton());
 }
