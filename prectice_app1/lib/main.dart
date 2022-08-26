3a58b0d8d912889b755f46774be12a85a2386d1d
sajjad-njr <maxsajjad29@gmail.com>
2022-08-26 14:44:55 +0600

    Navigation bar done

--- prectice_app1/lib/main.dart
+++ prectice_app1/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'appBarCreate.dart';
+//import 'appBarCreate.dart';
+//
+import 'botomNavicreate.dart';
 
 void main() {
-  runApp(const appBarCreate());
+  //runApp(const appBarCreate()); // ----------- 1st ---- AppBar added
+  runApp(botomNavicreate());
 }
