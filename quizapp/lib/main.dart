17c8bce7cb44552e75950dee877fbe1e205034cc
sajjad-njr <maxsajjad29@gmail.com>
2022-08-28 23:53:11 +0600

    quiz app

---
+++ quizapp/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
