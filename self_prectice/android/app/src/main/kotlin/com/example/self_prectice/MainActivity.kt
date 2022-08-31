4ed0b6674073618fc2decc35374bf9df816cbcec
sajjad-njr <maxsajjad29@gmail.com>
2022-09-01 02:43:02 +0600

    Bismillah . just started basic self practice

---
+++ self_prectice/android/app/src/main/kotlin/com/example/self_prectice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.self_prectice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ self_prectice/lib/1_AppBar/MyApp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
