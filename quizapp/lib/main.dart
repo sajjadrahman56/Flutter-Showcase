1f8ad408fde6312827d0f6fd4d11d6979ecc76d9
sajjad-njr <maxsajjad29@gmail.com>
2022-08-31 23:24:18 +0600

    create stateless class and return simple Material with home parameter

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //if i return MaterialApp at first I should mention home paremeter
+    return MaterialApp(
+      home: Text(" Amigo SaS we are here "),
+    );
+  }
+}
