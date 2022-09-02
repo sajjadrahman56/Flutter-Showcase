4edd213af4de97b9caecb2aae41e5988fcc49567
sajjad-njr <maxsajjad29@gmail.com>
2022-09-02 23:17:02 +0600

    second app bar added

---
+++ prectice_app1/lib/1_appBar/SecondAppBar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SecondAppBar extends StatelessWidget {
+  const SecondAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.people),
+          leadingWidth: 50,
+          toolbarHeight: 70,
+          toolbarOpacity: 0.8,
+          title: Text("Amigo"),
+          actions: [
+            IconButton(
+              tooltip: 'Ami Kala',
+              splashRadius: 20,
+              splashColor: Color.fromARGB(255, 16, 15, 15),
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text("this is me")));
+              },
+              icon: Icon(
+                Icons.man,
+                size: 40,
+              ),
+              color: Colors.red,
+            ),
+            IconButton(
+              tooltip: 'Ami Nil',
+              splashRadius: 20,
+              splashColor: Color.fromARGB(255, 16, 15, 15),
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text("this is me")));
+              },
+              icon: Icon(
+                Icons.man,
+                size: 40,
+              ),
+              color: Color.fromARGB(255, 83, 9, 201),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- prectice_app1/lib/main.dart
+++ prectice_app1/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:prectice_app1/1_appBar/SecondAppBar.dart';
 
 import '1_appBar/MyApp.dart';
 
 void main() {
   // runApp(const appBarCreate()); // ----------- 1st ---- AppBar added
   // runApp(botomNavicreate()); //-------
-  runApp(MyApp()); //------
+  runApp(SecondAppBar()); //------
 }
