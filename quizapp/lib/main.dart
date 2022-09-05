6b97e8d5d01e48250b2a79843ed9fc8b1ba220a8
sajjad-njr <maxsajjad29@gmail.com>
2022-09-05 10:54:49 +0600

    Question change but not repeat when I touch last index

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import '1_button/MyApp.dart';
+//import '1_button/MyApp.dart';
+import '1_button/StateChange.dart';
 // void main() {
 //   runApp(MyApp());
 // }
 
-void main() => runApp(MyApp());
+void main() => runApp(StateChange());
