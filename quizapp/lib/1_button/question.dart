e838ddfd3527c46f62edbd02084cbe39ed87d2c2
sajjad-njr <maxsajjad29@gmail.com>
2022-09-05 11:55:37 +0600

    more parameter add in the Container

--- quizapp/lib/1_button/question.dart
+++ quizapp/lib/1_button/question.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
   String qustionText;
@@ -6,9 +7,22 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //A Container class can be used to store one or more widgets and position them on the screen
+    //according to our convenience.
+    //A basic container element that stores a widget has a margin,
+    //which separates the present container from other contents.
+    //The total container can be given a border of different shapes,
+    //for example, rounded rectangles, etc.
+    //A container surrounds its child with padding and
+    //then applies additional constraints to the padded extent
+
     return Container(
+      //all of peremter about CONTAINER
+      height: 80,
       width: double.infinity,
+      alignment: Alignment.center,
       margin: EdgeInsets.all(10),
+      color: Colors.purple,
       child: Text(
         qustionText,
         style: TextStyle(fontSize: 28),
