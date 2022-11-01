a5f3e8642acd434e6449f103b68e48aff1f1b8f5
sajjad-njr <maxsajjad29@gmail.com>
2022-11-02 00:19:43 +0600

    fb task

--- list_view_more/lib/assignment/homePage.dart
+++ list_view_more/lib/assignment/homePage.dart
@@ -9,38 +9,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _ipgot = 'a';
+  String _ipgot = 'a', _isp = 'l', _country = 'Bangladesh', _div = 'h';
+
+  void getRequestTest() async {
+    Uri url = Uri.https("ifconfig.co", "json");
+    sajjad.Response response = await sajjad.get(url);
+    //print(response.body);
+
+    Map<String, dynamic> json = jsonDecode(response.body);
+
+    print(json["ip"]);
+
+    setState(() {
+      _ipgot = json["ip"];
+      _isp = json["asn_org"];
+      _div = json["city"];
+      _country = _div + ',' + 'Bangladesh';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    void getRequestTest() async {
-      Uri url = Uri.https("ifconfig.co", "json");
-      sajjad.Response response = await sajjad.get(url);
-      //print(response.body);
-
-      Map<String, dynamic> json = jsonDecode(response.body);
-      setState(() {
-        _ipgot = json["ip"];
-      });
-      print(json["ip"]);
-    }
-
-    TextStyle textStyle = const TextStyle(
-      fontSize: 18,
-      height:
-          2, //line height 200%, 1= 100%, were 0.9 = 90% of actual line height
-      // color: Colors.redAccent, //font color
-      //backgroundColor: Colors.black12, //background color
-      //letterSpacing: 5, //letter spacing
-      //decoration: TextDecoration.underline, //make underline
-      // decorationStyle: TextDecorationStyle.double, //double underline
-      // decorationColor: Colors.brown, //text decoration 'underline' color
-      //decorationThickness: 1.5, //decoration 'underline' thickness
-      //fontStyle: FontStyle.italic,
-    );
     return Scaffold(
       appBar: AppBar(
-        title: Center(
+        title: const Center(
           child: Text("Network Info "),
         ),
       ),
@@ -59,12 +51,12 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
             ),
             // const SizedBox(height: 10,),
-            textPass(_ipgot),
+            textPass(_isp),
             const Text(
               "Current Location",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
             ),
-            BoxDesign(_ipgot),
+            textPass(_country),
             const SizedBox(
               height: 10,
             ),
@@ -72,42 +64,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: () {
                   getRequestTest();
                 },
-                child:
-                    Text("Show on google map", style: TextStyle(fontSize: 19))),
+                child: Text("Show on map", style: TextStyle(fontSize: 19))),
           ],
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
-  Widget BoxDesign(String text) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      margin: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.blue,
-            width: 5,
-          ),
-          borderRadius: BorderRadius.circular(50)),
-      child: const Center(
-          child: Text(
-        "a",
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
-      )),
-    );
-  }
-
-  // void getRequestTest() async {
-  //   Uri url = Uri.https("ifconfig.co", "json");
-  //   sajjad.Response response = await sajjad.get(url);
-  //   //print(response.body);
-
-  //   Map<String, dynamic> json = jsonDecode(response.body);
-  //   _ipgot = json["ip"];
-  //   print(json["ip"]);
-  // }
 }
 
 class textPass extends StatelessWidget {
@@ -128,7 +91,11 @@ class textPass extends StatelessWidget {
       child: Center(
         child: Text(
           childText,
-          style: TextStyle(fontSize: 16),
+          style: const TextStyle(
+              fontSize: 16,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1),
         ),
       ),
     );
