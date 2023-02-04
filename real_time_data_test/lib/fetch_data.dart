8798ff66231cb084a14e933408d4119284f07471
sajjad-njr <maxsajjad29@gmail.com>
2023-02-04 20:49:15 +0600

    update real time data fixed

--- real_time_data_test/lib/fetch_data.dart
+++ real_time_data_test/lib/fetch_data.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 import 'package:real_time_data_test/update_record.dart';
 
 class FetchData extends StatefulWidget {
@@ -14,81 +13,91 @@ class FetchData extends StatefulWidget {
 }
 
 class _FetchDataState extends State<FetchData> {
-  Query dbRef = FirebaseDatabase.instance.ref().child('Students');
-  DatabaseReference reference =
-      FirebaseDatabase.instance.ref().child('Students');
+  Query dbRef = FirebaseDatabase.instance.ref().child('News');
+  DatabaseReference reference = FirebaseDatabase.instance.ref().child('News');
 
   Widget listItem({required Map student}) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.all(10),
-      height: 120,
-      color: Color.fromARGB(255, 64, 255, 172),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            student['name'],
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            student['age'],
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            student['salary'],
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+    return Column(children: [
+      Container(
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
+        height: 170,
+        color: Color.fromARGB(255, 64, 255, 172),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) =>
-                                UpdateRecord(studentKey: student['key'])));
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.edit,
-                        color: Theme.of(context).primaryColor,
-                      )
-                    ],
-                  )),
-              SizedBox(
-                width: 6,
+              Text(
+                student['name'],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
               ),
-              GestureDetector(
-                  onTap: () {
-                    reference.child(student['key']).remove();
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.delete,
-                        color: Colors.red[700],
-                      )
-                    ],
-                  )),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                student['age'],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                student['salary'],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                student['story'],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) =>
+                                    UpdateRecord(newsKey: student['key'])));
+                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.edit,
+                            color: Theme.of(context).primaryColor,
+                          )
+                        ],
+                      )),
+                  SizedBox(
+                    width: 6,
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        reference.child(student['key']).remove();
+                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.delete,
+                            color: Colors.red[700],
+                          )
+                        ],
+                      )),
+                ],
+              )
             ],
-          )
-        ],
+          ),
+        ),
       ),
-    );
+    ]);
   }
 
   @override
--- real_time_data_test/lib/update_record.dart
+++ real_time_data_test/lib/update_record.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class UpdateRecord extends StatefulWidget {
-  const UpdateRecord({Key? key, required this.studentKey}) : super(key: key);
-  final String studentKey;
+  const UpdateRecord({Key? key, required this.newsKey}) : super(key: key);
+  final String newsKey;
 
   @override
   State<UpdateRecord> createState() => _UpdateRecordState();
@@ -17,24 +13,26 @@ class _UpdateRecordState extends State<UpdateRecord> {
   final userNameCrontroller = TextEditingController();
   final userAgeCrontroller = TextEditingController();
   final userSalaryCrontroller = TextEditingController();
+  final storyController = TextEditingController();
 
   late DatabaseReference dbRef;
 
   @override
   void initState() {
     super.initState();
-    dbRef = FirebaseDatabase.instance.ref().child('Students');
+    dbRef = FirebaseDatabase.instance.ref().child('News');
     getStudentData();
   }
 
   void getStudentData() async {
-    DataSnapshot snapshot = await dbRef.child(widget.studentKey).get();
+    DataSnapshot snapshot = await dbRef.child(widget.newsKey).get();
 
     Map student = snapshot.value as Map;
 
     userNameCrontroller.text = student['name'];
     userAgeCrontroller.text = student['age'];
     userSalaryCrontroller.text = student['salary'];
+    storyController.text = student['story'];
   }
 
   @override
@@ -43,78 +41,92 @@ class _UpdateRecordState extends State<UpdateRecord> {
       appBar: AppBar(
         title: Text('Update Record'),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(9.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                'Updating Data in Firebase Realtime Data',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(9.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextField(
-                controller: userNameCrontroller,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Name',
-                    hintText: 'Enter your Name'),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextField(
-                controller: userNameCrontroller,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'AGE',
-                    hintText: 'Enter your AGE'),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextField(
-                controller: userNameCrontroller,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Salary',
-                    hintText: 'Enter your Salary'),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              MaterialButton(
-                onPressed: () {
-                  Map<String, String> students = {
-                    'name': userNameCrontroller.text,
-                    'age': userAgeCrontroller.text,
-                    'salary': userSalaryCrontroller.text
-                  };
+                const Text(
+                  'Updating Data in Firebase Realtime Data',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  controller: userNameCrontroller,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Name',
+                      hintText: 'Enter your Name'),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  controller: userNameCrontroller,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'AGE',
+                      hintText: 'Enter your AGE'),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  controller: userNameCrontroller,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Salary',
+                      hintText: 'Enter your Salary'),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  controller: storyController,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'story',
+                      hintText: 'Enter your Salary'),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Map<String, String> students = {
+                      'name': userNameCrontroller.text,
+                      'age': userAgeCrontroller.text,
+                      'salary': userSalaryCrontroller.text,
+                      'story': storyController.text
+                    };
 
-                  dbRef
-                      .child(widget.studentKey)
-                      .update(students)
-                      .then((value) => {Navigator.pop(context)});
-                },
-                child: Text('Update Data'),
-                color: Colors.amber,
-                textColor: Colors.black,
-                minWidth: 400,
-                height: 400,
-              )
-            ],
+                    dbRef
+                        .child(widget.newsKey)
+                        .update(students)
+                        .then((value) => {Navigator.pop(context)});
+                  },
+                  child: Text('Update Data'),
+                  color: Colors.amber,
+                  textColor: Colors.black,
+                  minWidth: 400,
+                  height: 400,
+                )
+              ],
+            ),
           ),
         ),
       ),
